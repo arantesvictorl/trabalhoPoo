f108c5c0b1776c2e971b01f2947f0d98ee212db0
Victor <victorlsarantes@gmail.com>
2023-01-12 08:58:14 -0300

    evolucao

--- Empresa.py
+++ Empresa.py
@@ -1,12 +1,49 @@
 from Usuario import Usuario
 
 class Empresa(Usuario):
-    def __init__(self, nome, senha, cnpj, contatos, infoPagamento, servicos, mercado, tempoMercado, filosofia, tamanho) -> None:
+    def __init__(self, nome, senha, cnpj, contatos, infoPagamento, ramoAtuacao, esteticaVisual, publicoAlvo, produtoPrincipal) -> None:
         super().__init__(nome, senha, cnpj, contatos, infoPagamento)
-        self.__servicos = servicos
-        self.__mercado = mercado
-        self.__tempoMercado = tempoMercado
-        self.__filosofia =  filosofia
-        self.__tamanho = tamanho
+        self.__ramoAtuacao = ramoAtuacao
+        self.__esteticaVisual = esteticaVisual
+        self.__publicoAlvo =  publicoAlvo
+        self.__produtoPrincipal = produtoPrincipal
+
+    @property
+    def getramoAtuacao(self):
+        return self.__ramoAtuacao
+
+    @getramoAtuacao.setter
+    def setramoAtuacao(self, ramoAtuacao):
+        self.__ramoAtuacao = ramoAtuacao
+        
+    @property
+    def getesteticaVisual(self):
+        return self.__esteticaVisual
+
+    @getesteticaVisual.setter
+    def setesteticaVisual(self, esteticaVisual):
+        self.__esteticaVisual = esteticaVisual
+    
+    @property
+    def getpublicoAlvo(self):
+        return self.__publicoAlvo
+    
+    @getpublicoAlvo.setter
+    def setpublicoAlvo(self, publicoAlvo):
+        self.__publicoAlvo = publicoAlvo
+
+    @property
+    def getprodutoPrincipal(self):
+        return self.__produtoPrincipal
+
+    @getprodutoPrincipal.setter
+    def setprodutoPrincipal(self, produtoPrincipal):
+        self.__produtoPrincipal = produtoPrincipal
+
+
+
+
+        
+
 
         
--- Usuario.py
+++ Usuario.py
@@ -6,9 +6,45 @@ class Usuario():
         self.__contatos = contatos
         self.__infoPagamento = infoPagamento
 
-    def verificaLogin():
-        pass
-    def atualizarInformacoes():
-        pass
-    def contratServ():
-        pass
+    @property
+    def getNome(self):
+        return self.__nome
+
+    @getNome.setter
+    def setNome(self, nome):
+        self.__nome = nome
+
+    @property
+    def getSenha(self):
+        return self.__senha
+
+    @getSenha.setter
+    def setSenha(self, senha):
+        self.__senha = senha
+    
+    @property
+    def getCnpj(self):
+        return self.__cnpj
+    
+    @getCnpj.setter
+    def setCnpj(self, cnpj):
+        self.__cnpj = cnpj
+
+    @property
+    def getContatos(self):
+        return self.__contatos
+
+    @getContatos.setter
+    def setContatos(self, contatos):
+        self.__contatos = contatos
+
+    @property
+    def getInfoPagamento(self):
+        return self.__infoPagamento
+    
+    @getInfoPagamento.setter
+    def setInfoPagamento(self, infoPagamento):
+        self.__infoPagamento = infoPagamento
+    
+
+
